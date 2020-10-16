3660782b3f9f665a1301d3b1b030c72316acdd7d
NamDell <namkhanh.nguyen@edu.sait.ca>
2020-10-16 12:08:50 -0600

    Setup express

---
+++ routes/bootcamps.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
+
+const router = express.Router();
+
+
+
+module.exports = router;
